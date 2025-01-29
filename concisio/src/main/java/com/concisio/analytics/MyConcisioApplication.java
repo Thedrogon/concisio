71ece41e43dfce9d598d933226ef8a100b75dd81
Sayanjit Mukherjee <sayanjitmukherjee123@gmail.com>
2025-01-29 23:16:23 +0530

    intialising project

---
+++ concisio/src/main/java/com/concisio/analytics/MyConcisioApplication.java
@@ -0,0 +1,13 @@
+package com.concisio.analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyConcisioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyConcisioApplication.class, args);
+	}
+
+}
---
+++ concisio/src/test/java/com/concisio/analytics/MyConcisioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concisio.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyConcisioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
