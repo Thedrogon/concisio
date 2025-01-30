ab382b8bc36c826588ac24e23898ec1a4c43fdd7
Sayanjit Mukherjee <sayanjitmukherjee123@gmail.com>
2025-01-30 10:32:28 +0530

    making changes

--- src/main/java/com/concisio/analytics/models/ClickEvent.java
+++ src/main/java/com/concisio/analytics/models/ClickEvent.java
@@ -1,4 +1,17 @@
 package com.concisio.analytics.models;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
 public class ClickEvent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime clickDate;
+
+
+    @ManyToOne
+    @JoinColumn(name = "url_mapping_id")
+    private UrlMapping urlMapping;
 }
